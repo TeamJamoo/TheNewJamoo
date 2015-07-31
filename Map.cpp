4d536cbc6fa129da7da0a5db891b414538d84460
holgater <richardjholgate@gmail.com>
2015-07-31 16:30:49 -0700

    Fifth Commit: started generate_map in Map.cpp/Map,h

--- Map.cpp
+++ Map.cpp
@@ -1,2 +1,18 @@
 //Author: Richard Holgate
 //Last Updated: 7/31/2015
+
+#include "map.h"
+
+void Map:generate_map(int width, int height)
+{
+	//create empty map
+	map_tiles = new Tile*[width];
+	for (int i = 0; i < width; ++i)
+	{
+		map_tiles[i] = Tile[height];
+	}
+
+	//create the first room
+	this.create_room(1,1,3,3);
+
+}
