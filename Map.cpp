3ed45f04ace8fb4c39fbe76dc30a7cab00cf1045
KDrab <kennydrabble1994@gmail.com>
2015-07-31 18:11:26 -0700

    Seventh Commit: work on generate rooms

--- Map.cpp
+++ Map.cpp
@@ -23,6 +23,13 @@ void Map::create_rooms()
 	//the x and y of the room being generated
 	int room_x_pos = 1;
 	int room_y_pos = 1;
+	//height and width of basic room
+	int height = 3;
+	int width = 3;
+	//the position of the door
+	int door_x_pos;
+	int door_y_pos;
+	
 	//which side of the room the current door is being generated on
 	//(1 = left, 2 = top, 3 = right, 4 = bottom)
 	int door_side;
@@ -35,5 +42,27 @@ void Map::create_rooms()
 		create_room(room_x_pos,room_y_pos,3,3);
 		//choose a random tile for a door to be placed
 		door_side = rand() % 4 + 1;
+		//if door_side = 1, door is on the left side
+		if (door_side == 1) {
+			door_x_pos = room_x_pos - 1;
+			door_y_pos = rand() % height + room_y_pos; 
+		} else 
+		//if door_side = 2, door is on the top side
+		if (door_side == 2) {
+			door_x_pos = rand() % room_x_pos + width;
+			door_y_pos = room_y_pos - 1;
+
+		} else
+		//if door_side = 3, door is on the right side
+		if (door_side == 3) {
+			door_x_pos = room_x_pos + height;
+			door_y_pos = rand() room_y_pos + height;
+		} else
+		//if door_side = 4, door is on the bottom side
+		if (door_side == 4) {
+			door_x_pos = room_x_pos + width;
+			door_y_pos = room_y_pos +height;
+
+		}		
 	}
 }
